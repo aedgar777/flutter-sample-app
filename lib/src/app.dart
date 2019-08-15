f9c14fa3b830259b9c9e8660c43a631c2a5f4fbd
Andrew Edgar <andrew@andrewedgar.io>
2019-08-15 16:58:56 -0400

    Beginning of the Navigation Section

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -6,7 +6,9 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StoriesProvider(
-      child: MaterialApp(title: 'News', home: NewsList()),
+      child: MaterialApp(title: 'News',
+          home: NewsList()),
+
     );
   }
 }
